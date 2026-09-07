1f638132ab2bdacd6dddb4060a72c12e4a0c484a
YoshikageKira425 <eneshalili425@gmail.com>
2026-09-07 20:20:16 +0200

    Made a simple view.

---
+++ src/game/views/game_view.py
@@ -0,0 +1,5 @@
+import arcade
+
+class GameView(arcade.View):
+    def __init__(self):
+        super().__init__(background_color=arcade.color.BLACK)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from ai-simulator!")
+import arcade
+from game.views.game_view import GameView 
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Car AI"
+
+def main():
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.show_view(GameView())
+    arcade.run()
 
 if __name__ == "__main__":
     main()
