7fbfcabfef7a891ee58af1edacd7272b2f3c31d8
YoshikageKira425 <eneshalili425@gmail.com>
2026-09-07 21:10:46 +0200

    Made a playable version to play with car, and fixed a bug with reverse function, where it didnt go reverse.

--- src/game/controller/car_controller.py
+++ src/game/controller/car_controller.py
@@ -5,42 +5,52 @@ from game.entity.car import Car
 class CarController:
     def __init__(self, car: Car):
         self.car = car
-        
+
         self.current_speed: float = 0.0
-        self.max_speed: float = 400.0  
-        self.max_reverse_speed: float = -150.0 
-        self.acceleration: float = 300.0  
-        self.friction: float = 0.98  
+        self.max_speed: float = 400.0
+        self.max_reverse_speed: float = -150.0
+        self.acceleration: float = 300.0
+        self.friction: float = 0.98
         self.turn_speed: float = 120.0
-        
+
     def forward(self, delta_time: float):
         self.current_speed += delta_time * self.acceleration
         if self.current_speed >= self.max_speed:
             self.current_speed = self.max_speed
-    
+
     def reverse(self, delta_time: float):
-        self.current_speed += delta_time * self.acceleration
+        self.current_speed -= delta_time * self.acceleration
         if self.current_speed <= self.max_reverse_speed:
             self.current_speed = self.max_reverse_speed
-    
+
     def turn_left(self, delta_time: float):
         if self.current_speed != 0:
             direction = 1.0 if self.current_speed > 0 else -1.0
-            self.car.angle += self.turn_speed * delta_time * direction
+            self.car.angle -= self.turn_speed * delta_time * direction
 
     def turn_right(self, delta_time: float):
         if self.current_speed != 0:
             direction = 1.0 if self.current_speed > 0 else -1.0
-            self.car.angle -= self.turn_speed * delta_time * direction
-    
-    def update(self, delta_time: float):
+            self.car.angle += self.turn_speed * delta_time * direction
+
+    def update(self, delta_time: float, actions: list[str]):
+        for action in actions:
+            if action == "forward":
+                self.forward(delta_time)
+            if action == "reverse":
+                self.reverse(delta_time)
+            if action == "left":
+                self.turn_left(delta_time)
+            if action == "right":
+                self.turn_right(delta_time)
+
         self.current_speed *= self.friction
-        
+
         if abs(self.current_speed) < 0.1:
             self.current_speed = 0.0
 
         rad = math.radians(self.car.angle)
-        change_x = -self.current_speed * math.sin(rad) * delta_time
+        change_x = self.current_speed * math.sin(rad) * delta_time
         change_y = self.current_speed * math.cos(rad) * delta_time
 
         self.car.center_x += change_x
--- src/game/views/game_view.py
+++ src/game/views/game_view.py
@@ -2,18 +2,42 @@ import arcade
 from game.entity.car import Car
 from game.controller.car_controller import CarController
 
+
 class GameView(arcade.View):
     def __init__(self):
         super().__init__(background_color=arcade.color.BLACK)
-        
+
         self.sprite_list = arcade.SpriteList(True)
-        
+
         self.car = Car()
         self.sprite_list.append(self.car)
-        
+
         self.car_controller = CarController(self.car)
-        
+        self.keys_held = []
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == arcade.key.W:
+            self.keys_held.append("forward")
+        if symbol == arcade.key.S:
+            self.keys_held.append("reverse")
+        if symbol == arcade.key.A:
+            self.keys_held.append("left")
+        if symbol == arcade.key.D:
+            self.keys_held.append("right")
+
+    def on_key_release(self, symbol, modifiers):
+        if symbol == arcade.key.W:
+            self.keys_held.remove("forward")
+        if symbol == arcade.key.S:
+            self.keys_held.remove("reverse")
+        if symbol == arcade.key.A:
+            self.keys_held.remove("left")
+        if symbol == arcade.key.D:
+            self.keys_held.remove("right")
+
+    def on_update(self, delta_time):
+        self.car_controller.update(delta_time, self.keys_held)
+
     def on_draw(self):
         self.clear()
         self.sprite_list.draw()
-        
