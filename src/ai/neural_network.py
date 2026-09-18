183c74ca7a37bb4a0116a115da21838885ac927f
YoshikageKira425 <eneshalili425@gmail.com>
2026-09-18 21:49:14 +0200

    Fixed the foward pass method.

--- src/ai/neural_network.py
+++ src/ai/neural_network.py
@@ -11,12 +11,11 @@ class NeuralNetwork:
         self.output_weight = numpy.random.randn(len(input), hidden_size) * 0.01
         self.output_bias = numpy.zeros((1, hidden_size))
         
-    def _tahn(self, x) -> float:
-        return (math.pow(math.e, x) - math.pow(math.e, -x)) / (math.pow(math.e, x) + math.pow(math.e, -x))
-    
     def foward_pass(self):
         z1 = numpy.dot(self.input, self.hidden_weight_1) + self.hidden_bias_1
-        a1 = numpy.tanh
+        a1 = numpy.tanh(z1)
         
         z2 = numpy.dot(self.output_weight, a1) * self.output_bias
-        y = self._tahn(z2)
+        y = numpy.tanh(z2)
+        
+        return y
