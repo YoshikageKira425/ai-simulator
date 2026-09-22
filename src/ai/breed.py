69e48773f7f19be5db9bf10d31d03cef293357c9
YoshikageKira425 <eneshalili425@gmail.com>
2026-09-22 19:49:25 +0200

    Made the mutations.

--- src/ai/breed.py
+++ src/ai/breed.py
@@ -4,6 +4,8 @@ from ..core.car_agent import CarAgent
 from ..model.neural_network_model import NeuralNetworkModel
 
 ELITISM = 2
+MUTATION_CHANCE = 0.1
+MUTATION_STRENGTH = 0.1
 
 def breed(agents: list[CarAgent], population:int = 20) -> list[NeuralNetworkModel]:
     results = []
@@ -15,7 +17,11 @@ def breed(agents: list[CarAgent], population:int = 20) -> list[NeuralNetworkMode
         parent_a = _tournament_selection(agents)
         parent_b = _tournament_selection(agents)
         
-        results.append(_crossover(parent_a, parent_b))
+        child = _crossover(parent_a, parent_b)
+        
+        mutated_child = _mutate(child)
+        
+        results.append(mutated_child)
         
     return results
         
@@ -23,6 +29,23 @@ def _tournament_selection(agents: list[CarAgent]) -> NeuralNetworkModel:
     k_agents = random.sample(agents, k=3)
     
     return max(k_agents, key=lambda agent: agent.fitness).brain
+
+def _mutate(child: NeuralNetworkModel):
+    if random.random() > MUTATION_CHANCE:
+        return
+    
+    def mutate_matrix(matrix: numpy.ndarray) -> numpy.ndarray:
+        mask = numpy.random.rand(*matrix.shape) < MUTATION_CHANCE
+        noise = numpy.random.randn(*matrix.shape) * MUTATION_STRENGTH
+        
+        return matrix + (mask * noise)
+
+    return NeuralNetworkModel(
+        hidden_weight1=mutate_matrix(child.hidden_weight1),
+        hidden_bias1=mutate_matrix(child.hidden_bias1),
+        output_weight2=mutate_matrix(child.output_weight2),
+        output_bias2=mutate_matrix(child.output_bias2),
+    )
 
 def _crossover(parent_a: NeuralNetworkModel, parent_b: NeuralNetworkModel) -> NeuralNetworkModel:
     hidden_weight_1 = (parent_a.hidden_weight_1 + parent_b.hidden_weight_1) / 2
