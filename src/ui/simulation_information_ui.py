1e040a70933448bf4b14f256ca857947968f614a
YoshikageKira425 <eneshalili425@gmail.com>
2026-09-26 16:54:50 +0200

    Made some debugs options, like showing raycast and speeding the game.

--- src/ui/simulation_information_ui.py
+++ src/ui/simulation_information_ui.py
@@ -30,11 +30,22 @@ class SimulationInformationUi:
         )
         self._manager.add(self._time_remaining)
 
+        self._speed_lable = arcade.gui.UILabel(
+            "Speed: 1x",
+            x=15,
+            y=25,
+            font_size=18
+        )
+        self._manager.add(self._speed_lable)
+
     def set_alive_counter(self, alive_counter: int, max_alive_counter: int):
         self._alive_counter.text = f"Alive: {alive_counter} / {max_alive_counter}"
 
     def set_time_remaining(self, timer: float):
         self._time_remaining.text = f"Time Remaining: {round(timer, 2)}s"
+
+    def set_game_speed(self, speed: int):
+        self._speed_lable.text = f"Speed: {speed}x"
 
     def draw(self):
         self._manager.draw()
--- src/views/simulation_view.py
+++ src/views/simulation_view.py
@@ -18,15 +18,19 @@ class SimulationView(arcade.View):
         self.map_manager = MapManager(1)
         self.sprite_list.append(self.map_manager.get_map())
 
-        self.simulation_enviroment = SimulationEnvironment(self.map_manager, Checkpoint.load_generation())
+        self.simulation_enviroment = SimulationEnvironment(
+            self.map_manager, Checkpoint.load_generation())
         self.sprite_list.extend(self.simulation_enviroment.get_sprites())
+
+        self.debug_mode = False
+        self.game_speed = 1
 
         self.wait = 2
         self.kill_timer = KILL_TIMER
 
     def new_generation_spawn(self):
         Checkpoint.save_generation(self.simulation_enviroment.get_all_models())
-        
+
         for sprite in self.simulation_enviroment.get_sprites():
             self.sprite_list.remove(sprite)
 
@@ -36,15 +40,20 @@ class SimulationView(arcade.View):
         self.sprite_list.extend(self.simulation_enviroment.get_sprites())
 
     def on_update(self, delta_time):
+        for _ in range(self.game_speed):
+            self.step(delta_time)
+
+    def step(self, delta_time: float):
         self.ui.set_time_remaining(self.kill_timer)
-        self.ui.set_alive_counter(self.simulation_enviroment.active_agents_count, self.simulation_enviroment.population)
-        
+        self.ui.set_alive_counter(
+              self.simulation_enviroment.active_agents_count, self.simulation_enviroment.population)
+
         if self.wait > 0:
             self.wait -= delta_time
             return
 
         if self.simulation_enviroment.all_dead:
-            self.new_generation_spawn() 
+            self.new_generation_spawn()
             self.kill_timer = KILL_TIMER
 
         if self.kill_timer <= 0:
@@ -55,12 +64,27 @@ class SimulationView(arcade.View):
 
         self.simulation_enviroment.update(delta_time)
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == arcade.key.D:
+            self.debug_mode = not self.debug_mode
+
+        if symbol == arcade.key.SPACE:
+            if self.game_speed == 1:
+                self.game_speed = 2
+            elif self.game_speed == 2:
+                self.game_speed = 3
+            elif self.game_speed == 3:
+                self.game_speed = 1
+
+            self.ui.set_game_speed(self.game_speed)
+
     def on_draw(self):
         self.clear()
 
         self.sprite_list.draw()
-        
-        # for agent in self.simulation_enviroment.agents:
-                #agent.debug()
-        
+
+        if self.debug_mode:
+            for agent in self.simulation_enviroment.agents:
+                agent.debug()
+
         self.ui.draw()
